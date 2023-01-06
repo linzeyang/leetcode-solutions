5b8a4c6f81cb01e3b17ead37e3fb6581a16b2c7b
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-01-06 21:01:37 +0800

    Add solution on January 6th, 2023

---
+++ easy/0697.py
@@ -0,0 +1,28 @@
+"""697. Degree of an Array"""
+
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        cc = Counter(nums)
+        degree = max(cc.values())
+
+        if degree == 1:
+            return 1
+
+        targets = {num for num, val in cc.items() if val == degree}
+        temp = {}
+
+        for idx, num in enumerate(nums):
+            if num not in targets:
+                continue
+
+            if num not in temp:
+                temp[num] = [idx, None]
+            else:
+                temp[num][-1] = idx
+
+        return min(lis[-1] - lis[0] + 1 for lis in temp.values())
