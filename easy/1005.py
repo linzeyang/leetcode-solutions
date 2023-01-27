82bc1230f274d45d925b38d22c494c32dc44cd98
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-01-27 23:36:21 +0800

    Add solution on January 27th, 2023

---
+++ easy/1005.py
@@ -0,0 +1,36 @@
+""""""
+
+from typing import List
+
+
+class Solution:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        minn = min(nums)
+
+        if minn >= 0:
+            if k % 2 == 0:
+                return sum(nums)
+
+            return sum(nums) - 2 * minn
+
+        nums = sorted(nums)
+
+        for idx, num in enumerate(nums):
+            if num < 0 and k > 0:
+                nums[idx] = -nums[idx]
+                k -= 1
+            else:
+                break
+
+        if k == 0 or k % 2 == 0:
+            return sum(nums)
+
+        if nums[idx] == 0:
+            return sum(nums)
+
+        if idx + 1 < len(nums):
+            return sum(nums) - 2 * min(
+                abs(nums[idx - 1]), abs(nums[idx]), abs(nums[idx + 1])
+            )
+
+        return sum(nums) - 2 * min(abs(nums[idx - 1]), abs(nums[idx]))
