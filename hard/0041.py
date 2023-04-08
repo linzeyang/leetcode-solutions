8e4123d20aeb469a7bc34f45c40d7a72d8711e49
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-04-08 21:15:18 +0800

    Add solutions on April 8th, 2023

---
+++ hard/0041.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+
+        i = 1
+
+        while i in nums_set:
+            i += 1
+
+        return i
