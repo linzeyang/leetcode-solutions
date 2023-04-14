a4eb994872718069fbf4a7efab64da10e047fe3e
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-04-14 21:00:38 +0800

    Add JS solutions on April 14th, 2023

---
+++ easy/2629.js
@@ -0,0 +1,24 @@
+// 2629. Function Composition
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+
+var compose = function (functions) {
+    var fun = function (num) {
+
+        for (let i = 0; i < functions.length; i++) {
+            num = functions[functions.length - 1 - i](num)
+        }
+
+        return num;
+    }
+
+    return fun;
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
