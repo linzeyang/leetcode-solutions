8149e904a4815f2cc63987dc99eb6846c2fa0fa0
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-05-06 22:26:19 +0800

    Add JS solutions on May 6th, 2023

---
+++ easy/2666.js
@@ -0,0 +1,27 @@
+// 2666. Allow One Function Call
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function (fn) {
+  let called = false;
+
+  return function (...args) {
+    if (!called) {
+      let result = fn(...args);
+      called = true;
+      return result;
+    }
+
+    return undefined;
+  };
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
