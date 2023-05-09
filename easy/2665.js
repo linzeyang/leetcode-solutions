56e87e197dd8822d33b75730c444f50649ba83fb
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-05-09 20:20:20 +0800

    Add JS solution on May 9th, 2023

---
+++ easy/2665.js
@@ -0,0 +1,33 @@
+// 2665. Counter II
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+  let val = init;
+
+  const object = {
+    increment: function () {
+      val++;
+      return val;
+    },
+    reset: function () {
+      val = init;
+      return val;
+    },
+    decrement: function () {
+      val--;
+      return val;
+    },
+  };
+
+  return object;
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
