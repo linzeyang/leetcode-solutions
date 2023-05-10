3236d0b8bd5901ff70af0b5fd982a24b155aa215
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-05-10 20:29:08 +0800

    Add JS solution on May 10th, 2023

---
+++ easy/2649.js
@@ -0,0 +1,22 @@
+// 2649. Nested Array Generator
+
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function* (arr) {
+  for (let ele of arr) {
+    if (typeof ele === "number") {
+      yield ele;
+    } else {
+      yield* inorderTraversal(ele);
+    }
+  }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
