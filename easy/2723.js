89fa0c12dfdfc4010f702138a0516109a0c7ce65
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-06-08 22:31:24 +0800

    Add JS solutions on June 8th, 2023

---
+++ easy/2723.js
@@ -0,0 +1,21 @@
+// 2723. Add Two Promises
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+  return new Promise((resolve, reject) => {
+    promise1.then((num1) => {
+      promise2.then((num2) => {
+        resolve(num1 + num2);
+      });
+    });
+  });
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
