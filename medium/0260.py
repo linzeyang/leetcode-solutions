0502fa2170135d94686db0e4bd616b031e8c688b
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-06-14 23:49:23 +0800

    Add solution on June 14th, 2023

---
+++ medium/0260.py
@@ -0,0 +1,11 @@
+"""260. Single Number III"""
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        cc = Counter(nums)
+
+        return list(num for num, count in cc.items() if count == 1)
