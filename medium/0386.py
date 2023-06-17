7010c0c25216aac7c302486cf7121f590d4b2536
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-06-17 20:46:07 +0800

    Add solutions on June 17th, 2023

---
+++ medium/0386.py
@@ -0,0 +1,36 @@
+"""386. Lexicographical Numbers"""
+
+from typing import List
+
+
+# This runs faster
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        return sorted(range(1, n + 1), key=str)
+
+
+# This runs slower
+"""
+from functools import cmp_to_key
+
+
+def comp(lhs: int, rhs: int) -> int:
+    l_str = str(lhs)
+    r_str = str(rhs)
+
+    for idx in range(min(len(l_str), len(r_str))):
+        if l_str[idx] < r_str[idx]:
+            return -1
+        elif l_str[idx] > r_str[idx]:
+            return 1
+
+    if len(l_str) < len(r_str):
+        return -1
+
+    return 1
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        return sorted(range(1, n + 1), key=cmp_to_key(comp))
+
+"""
---
+++ medium/0398.py
@@ -0,0 +1,23 @@
+"""398. Random Pick Index"""
+
+from random import choice
+from typing import List
+
+
+class Solution:
+    def __init__(self, nums: List[int]):
+        self._con = {}
+
+        for idx, num in enumerate(nums):
+            if num not in self._con:
+                self._con[num] = [idx]
+            else:
+                self._con[num].append(idx)
+
+    def pick(self, target: int) -> int:
+        return choice(self._con[target])
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
