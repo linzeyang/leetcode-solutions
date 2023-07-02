b77ececee4c4c030421c00f8ed98204926ca6a36
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-07-02 22:45:29 +0800

    Add solution on July 2nd, 2023

---
+++ medium/2225.py
@@ -0,0 +1,15 @@
+"""2225. Find Players With Zero or One Losses"""
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        lis1 = sorted(set(win for win, _ in matches) - set(lose for _, lose in matches))
+
+        cc = Counter(lose for _, lose in matches)
+
+        lis2 = sorted(lose for lose, count in cc.items() if count == 1)
+
+        return [lis1, lis2]
