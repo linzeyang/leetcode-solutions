a3c0cdc3b9bb7d07e678a7c7add4ccd83a76bbfa
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-07-27 22:20:31 +0800

    Add solution on July 27th, 2023

---
+++ medium/2785.py
@@ -0,0 +1,21 @@
+"""2785. Sort Vowels in a String"""
+
+
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowels: list[str] = []
+        vowel_indexes: list[int] = []
+
+        VOS = "aeiou"
+
+        for idx, letter in enumerate(s):
+            if letter.lower() in VOS:
+                vowels.append(letter)
+                vowel_indexes.append(idx)
+
+        s_list = list(s)
+
+        for idx, letter in zip(vowel_indexes, sorted(vowels)):
+            s_list[idx] = letter
+
+        return "".join(s_list)
