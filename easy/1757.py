cbfb0fe89e86637e8098716f33aee8984b1ba1a4
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-08-02 23:42:15 +0800

    Add Pandas solutions on August 2nd, 2023

---
+++ easy/1757.py
@@ -0,0 +1,9 @@
+"""1757. Recyclable and Low Fat Products"""
+
+import pandas as pd
+
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products[(products["low_fats"] == "Y") & (products["recyclable"] == "Y")][
+        ["product_id"]
+    ]
