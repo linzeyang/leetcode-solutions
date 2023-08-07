1498569d62cecc024a2702fc75c19df552d865c5
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-08-07 21:26:35 +0800

    Add Pandas solutions on August 7th, 2023

---
+++ easy/0196.py
@@ -0,0 +1,9 @@
+"""196. Delete Duplicate Emails"""
+
+import pandas as pd
+
+
+# Modify Person in place
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values("id", inplace=True)
+    person.drop_duplicates(subset="email", keep="first", inplace=True)
---
+++ easy/1667.py
@@ -0,0 +1,9 @@
+"""1667. Fix Names in a Table"""
+
+import pandas as pd
+
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    users["name"] = users["name"].str.title()
+
+    return users.sort_values("user_id")
