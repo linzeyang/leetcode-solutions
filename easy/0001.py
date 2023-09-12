8a820e7aca75bafcf838105511c545ffc60cfdb3
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-09-12 17:42:40 +0800

    Update solution on September 12th, 2023

--- easy/0001.py
+++ easy/0001.py
@@ -1,14 +1,22 @@
-"""
-1. Two Sum
-"""
+"""1. Two Sum"""
+
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range((length := len(nums)) - 1):
-            b = target - nums[i]
+        mapping: dict[int, int] = {}
 
-            for j in range(i + 1, length):
-                if nums[j] == b:
-                    return [i, j]
+        for idx, num in enumerate(nums):
+            if (remaining := target - num) not in mapping:
+                mapping[num] = idx
+            else:
+                return [mapping[remaining], idx]
+
+        return []
+
+
+def test_solution():
+    assert Solution().twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert Solution().twoSum([2, 7, 11, 15], 26) == [2, 3]
+    assert Solution().twoSum([3, 3], 6) == [0, 1]
