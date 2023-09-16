d6cbdcbfbf8341f6aec8784820bd4f023ff5f9ae
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-09-16 23:11:22 +0800

    Add solutions on September 16th, 2023

--- easy/0283.py
+++ easy/0283.py
@@ -1,6 +1,5 @@
-"""
-283. Move Zeroes
-"""
+"""283. Move Zeroes"""
+
 from typing import List
 
 
@@ -9,17 +8,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Slow:
-        # Runtime: 378 ms, faster than 28.34% of Python3 online submissions for Move Zeroes.
-        # Memory Usage: 15.6 MB, less than 64.75% of Python3 online submissions for Move Zeroes.
-        zeroes_found = 0
-        idx = 0
+        fast = slow = 0
 
-        while idx < len(nums) - zeroes_found:
-            if nums[idx] != 0:
-                idx += 1
-                continue
-
-            nums.pop(idx)
-            nums.append(0)
-            zeroes_found += 1
+        while fast < len(nums):
+            if nums[fast] == 0:
+                fast += 1
+            else:
+                if fast != slow:
+                    nums[fast], nums[slow] = nums[slow], nums[fast]
+                fast += 1
+                slow += 1
---
+++ medium/0912.py
@@ -0,0 +1,8 @@
+"""912. Sort an Array"""
+
+from typing import List
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return sorted(nums)
