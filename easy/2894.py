8a27fd2cd0f528538d05c1f51441aabc1d9a9bbc
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-10-13 22:49:55 +0800

    Add solution on October 13th, 2023

---
+++ easy/2894.py
@@ -0,0 +1,15 @@
+"""2894. Divisible and Non-divisible Sums Difference"""
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        # return sum(range(1, n+1)) - 2 * sum(range(0, n + 1, m))
+
+        num1 = num2 = 0
+
+        for num in range(1, n + 1):
+            if num % m:
+                num1 += num
+            else:
+                num2 += num
+
+        return num1 - num2
