7fa7e8d3b6e6b594e55e5e7a8facc63f53257e06
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2023-11-04 21:30:37 +0800

    Add solution on November 4th, 2023

---
+++ easy/2917.py
@@ -0,0 +1,18 @@
+"""2917. Find the K-or of an Array"""
+
+from typing import List
+
+
+class Solution:
+    def findKOr(self, nums: List[int], k: int) -> int:
+        num_bits = len(f"{max(nums):b}")
+        out = 0
+
+        for bit in range(num_bits + 1):
+            bit_2 = 2 ** bit
+            count = sum(1 for num in nums if (bit_2 & num) == bit_2)
+
+            if count >= k:
+                out += bit_2
+
+        return out
