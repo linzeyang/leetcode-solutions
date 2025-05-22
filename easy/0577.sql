0a18cb7186747b378d6d94ee692dbe95b64ba64f
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2025-05-22 15:33:25 +0800

    Add solutions on May 22nd, 2025

---
+++ easy/0577.sql
@@ -0,0 +1,11 @@
+-- 577. Employee Bonus
+
+SELECT
+    `Employee`.name,
+    `Bonus`.bonus
+FROM
+    `Employee`
+    LEFT JOIN `Bonus` ON `Employee`.empId = `Bonus`.empId
+WHERE
+    `Bonus`.bonus IS NULL
+    OR `Bonus`.bonus < 1000;
